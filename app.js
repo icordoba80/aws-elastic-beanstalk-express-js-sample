d861e28d6df2600fed9897c6dcb1b33dd113425a
Cordoba Ivan <Ivan.Cordoba@supervielle.com.ar>
2021-12-01 12:07:47 -0300

    FULL PIPELINE TEST! MY FIRST AWS PIPELINE

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Mundo!!'));
+app.get('/', (req, res) => res.send('Este es el cambio para el pipeline, mi primer pipeline en AWS!!!!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
