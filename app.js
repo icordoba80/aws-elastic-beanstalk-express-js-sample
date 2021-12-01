158efea9deb228f82a4ffcdea6ec68c07d9ebe98
Cordoba Ivan <Ivan.Cordoba@supervielle.com.ar>
2021-12-01 14:12:44 -0300

    mensaje uno

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-var os = require('os');
-var jsonText = '{' + os.EOL + '\t"text": "' + origText + '"' + os.EOL + '}';
 app.get('/', (req, res) => res.send('Reimon es mi Irmao, y Gonzo se la come! Gil Vouyerista xD'));
 
 app.listen(port);
