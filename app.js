30cd61c1e5b966980b4db9e7a0d4b55271483815
Cordoba Ivan <Ivan.Cordoba@supervielle.com.ar>
2021-12-01 14:48:55 -0300

    este deploy es para el bro

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-app.get('/', (req, res) => res.send('Reimon es mi Irmao, y Gonzo se la come! Gil Vouyerista xD'));
+app.get('/', (req, res) => res.send('HAY QUE VEEEEEEEERRRRR'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
