db2e2cd6ddb79eb21ea295ea1b6fdbe9ed5ee270
Cordoba Ivan <Ivan.Cordoba@supervielle.com.ar>
2021-12-01 12:19:20 -0300

    Cambio falopa pal reimin

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Este es el cambio para el pipeline, mi primer pipeline en AWS!!!!!!'));
+app.get('/', (req, res) => res.send('Reimon es mi Irmao, y Gonzo se la come! Gil Vouyerista xD'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
